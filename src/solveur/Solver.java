e53be49b2f6636b4a8c744640b7ea24e5223a08b
ElMastho <thomas.pichard67@gmail.com>
2020-01-10 11:39:43 +0100

    solution constructive

--- src/solveur/Solver.java
+++ src/solveur/Solver.java
@@ -1,75 +1,80 @@
 package solveur;
 
 import clustering.Point;
-
 import java.util.*;
 
 public class Solver {
     private static final String AUTHOR = "SCION Gael & PICHARD Thomas";
     private Parser parser;
-    private Object Integer;
 
-    Solver(String src){
-        parser = new Parser("test.dat");
-        chooseRandomClusters();
+    Solver(String src) {
+        parser = new Parser(src);
+        constructiveSolution();
     }
 
-    private void chooseRandomClusters(){
+    /**
+     * Solution constructive
+     */
+    private void constructiveSolution() {
         ArrayList<Point> points = parser.getPoints();
-        ArrayList<Point> pointsCopy = parser.getPoints();
         ArrayList<Boolean> yn = generateBooleanTab(parser.getNbPoints());
         ArrayList<ArrayList<Boolean>> zn = generateBooleanTab2D(parser.getNbPoints());
 
         HashSet<Integer> indCluster = new HashSet<>();
-        while(indCluster.size()!=parser.getNbCluster()){
-            int random = getRandomNumberInRange(0, yn.size());
+        while (indCluster.size() != parser.getNbCluster()) {
+            int random = getRandomNumberInRange(0, yn.size()-1);
             indCluster.add(random);
         }
         ArrayList<Integer> indCluster2 = new ArrayList<>(indCluster);
-        for(int i : indCluster2){
+        for (int i : indCluster2) {
             yn.set(i, true);
-            pointsCopy.remove(points.get(i));
         }
 
-        for(int cluster=0; cluster<indCluster2.size(); cluster++) {
-                    int nbPointByCluster = 0;
-                    if(cluster!=indCluster.size()-1){
-                        nbPointByCluster = Math.floorDiv( parser.getNbPoints() - parser.getNbCluster(), parser.getNbCluster());
-                    }
-                    else{
-                        nbPointByCluster = pointsCopy.size();
-                    }
-                    TreeMap<Double, Point> distInRelationToCluster = new TreeMap<>();
-                    for (Point po : pointsCopy) {
-                        distInRelationToCluster.put(points.get(indCluster2.get(cluster)).distBetweenTwoPoints(po), po);
-                    }
-                    Collection<Point> val = distInRelationToCluster.values();
-                    System.out.println(indCluster2.get(cluster)+" -> "+val);
-                    System.out.println("---------------------------------------------");
-                    Object[] array = val.toArray();
-                    for(int ch=0; ch<nbPointByCluster; ch++){
-                        Point p = (Point) array[ch];
-                        zn.get(points.indexOf(p)).set(indCluster2.get(cluster), true);
-                pointsCopy.remove(p);
+        for(Point p : points){
+            TreeMap<Double, Point> distInRelationToCluster = new TreeMap<>();
+            for(int i : indCluster2){
+                distInRelationToCluster.put(points.get(i).distBetweenTwoPoints(p), points.get(i));
             }
+            Collection<Point> val = distInRelationToCluster.values();
+            Object[] array = val.toArray();
+            Point poi = (Point) array[0];
+            zn.get(points.indexOf(p)).set(points.indexOf(poi), true);
         }
 
-        for(ArrayList<Boolean> z : zn){
-            System.out.println(z.contains(true));
+        toStringZn(zn);
+    }
+
+    private void toStringZn(ArrayList<ArrayList<Boolean>> zn){
+        for(int point=0; point<zn.size(); ++point){
+            if(point+1<10){
+                System.out.print((point+1)+"  [ ");
+            }
+            else{
+                System.out.print((point+1)+" [ ");
+            }
+            for(int pointC=0; pointC<zn.get(0).size(); ++pointC){
+                if(zn.get(point).get(pointC)){
+                    System.out.print("1 ");
+                }
+                else{
+                    System.out.print("_ ");
+                }
+            }
+            System.out.println("]");
         }
     }
 
-    private ArrayList<ArrayList<Boolean>> generateBooleanTab2D(int length){
+    private ArrayList<ArrayList<Boolean>> generateBooleanTab2D(int length) {
         ArrayList<ArrayList<Boolean>> tab = new ArrayList<>();
-        for(int i=0; i<length; ++i){
+        for (int i = 0; i < length; ++i) {
             tab.add(generateBooleanTab(length));
         }
         return tab;
     }
 
-    private ArrayList<Boolean> generateBooleanTab(int length){
+    private ArrayList<Boolean> generateBooleanTab(int length) {
         ArrayList<Boolean> tab = new ArrayList<>();
-        for(int i=0; i<length; ++i){
+        for (int i = 0; i < length; ++i) {
             tab.add(false);
         }
         return tab;
@@ -84,7 +89,7 @@ public class Solver {
         return r.nextInt((max - min) + 1) + min;
     }
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
         Solver s = new Solver("test.dat");
     }
 }
