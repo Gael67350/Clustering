abff75dd78c19831a9473e6c70ab121fdd2eb318
Gael67350 <gael67350@gmail.com>
2020-01-11 21:49:03 +0100

    Modification du générateur de dataset #1

--- src/clustering/ClusterCentre.java
+++ src/clustering/ClusterCentre.java
@@ -4,39 +4,27 @@ import java.util.Random;
 
 public class ClusterCentre extends Point {
     public static final int MIN_CLUSTER_AREA = 10;
-    public static final int MAX_CLUSTER_AREA = 50;
-
-    private double areaMin;
-    private double areaMax;
+    public static final int MAX_CLUSTER_AREA = 100;
 
     public ClusterCentre(int dimension) {
         super(dimension);
-
-        Random rand = new Random();
-        double areaLength = rand.nextDouble() * (MAX_CLUSTER_AREA - MIN_CLUSTER_AREA) + MIN_CLUSTER_AREA;
-
-        this.areaMin = coords.get(0) - (areaLength / 2.0);
-        this.areaMax = coords.get(0) + (areaLength / 2.0);
-
-        if (this.areaMin < Generator.MIN_ENV_AREA) {
-            this.areaMin = Generator.MIN_ENV_AREA;
-        }
-
-        if (this.areaMax > Generator.MAX_ENV_AREA) {
-            this.areaMax = Generator.MAX_ENV_AREA;
-        }
-    }
-
-    public double getAreaMin() {
-        return areaMin;
-    }
-
-    public double getAreaMax() {
-        return areaMax;
     }
 
     public Point getRandomPointInClusterArea() {
         Random rand = new Random();
+
+        double areaLength = rand.nextDouble() * (MAX_CLUSTER_AREA - MIN_CLUSTER_AREA) + MIN_CLUSTER_AREA;
+        double areaMin = coords.get(0) - (areaLength / 2.0);
+        double areaMax = coords.get(0) + (areaLength / 2.0);
+
+        if (areaMin < Generator.MIN_ENV_AREA) {
+            areaMin = Generator.MIN_ENV_AREA;
+        }
+
+        if (areaMax > Generator.MAX_ENV_AREA) {
+            areaMax = Generator.MAX_ENV_AREA;
+        }
+
         Point p = new Point(dimension);
 
         for (int i = 0; i < p.dimension; i++) {
